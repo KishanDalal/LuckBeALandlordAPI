dbca8b911906fb7ec8fc770aa62ea04e1c904c71
Kishan <ikishandalal@gmail.com>
2021-04-17 00:22:23 -0400

    Endpoints, Errorcode, Tested API
    Finished creating endpoints, finised adding error codes, changed scheme to have required, tested API ensuring it worked to spec.

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 const mongoose = require("mongoose")
 const express = require("express");
 const app = express();
+app.use(express.json());
 
 // specify the port that your server will run on
 const HTTP_PORT = process.env.PORT || 8083;
@@ -44,8 +45,8 @@ const onHttpStart = () => {
 // Create scehme for table
 // Define the table
 const ItemsSchema = new Schema({
-    name:String,
-    rarity:String,
+    name:{type: String, required: true},
+    rarity:{type: String, required: true},
     description:String,
     goldPerTurn:Number
  })
@@ -53,20 +54,62 @@ const ItemsSchema = new Schema({
  // Create the table and return the schema as a Mongoose model
  const Items = mongoose.model("items_table", ItemsSchema)
 
-// Endpoints
-/*
-const s3 = new Items({"name": "Kelly", "rarity": "common", "description": "LOL", "goldPerTurn":4})
-const s2 = new Items({"name": "Belly", "rarity": "zommon", "description": "LOL1", "goldPerTurn":0})
+
+/* Insert Values 
+const s3 = new Items({"name": "Magpie", "rarity": "common", "description": "Gives 9 gold per 4 spins", "goldPerTurn":-1})
+const s2 = new Items({"name": "King Midas", "rarity": "Rare", "description": "Adds 1 Gold each turn. Adjacent Gold gives 3x more gold", "goldPerTurn":2})
+const s5 = new Items({"name": "Goose", "rarity": "common", "description": "Has a 1% chance of adding a Golden Egg", "goldPerTurn":1})
+const s6 = new Items({"name": "Bee", "rarity": "uncommon", "description": "Adjacent Flowers give 2x more gold", "goldPerTurn":1})
+const s7 = new Items({"name": "Golden Egg", "rarity": "Rare", "description": "", "goldPerTurn":3})
+const s8 = new Items({"name": "Void Stone", "rarity": "uncommon", "description": "Adjacent empty squares give 1 coin more. Destorys itself if adacent to 0 empty squares. Give 8 coins when destroyed", "goldPerTurn":0})
 s3.save()
 s2.save()
+s5.save()
+s6.save()
+s7.save()
+s8.save()
 */
+
+
+// Status Codes
+
+var msg500 = {
+    statusCode: 500,
+    msg: "Error when find value"
+
+}
+
+var msg201 = {
+    statusCode: 201,
+    msg: "Success"
+}
+
+var msg200 = 
+{
+    statusCode: 200,
+    msg: "Ok, success"
+}
+
+var msg404 = 
+{
+    statusCode: 404,
+    msg: "Not Found"
+}
+
+var msg501 =
+{
+    statusCode: 501,
+    msg: "Not Implemented"
+}
+
+
+// Endpoints
 
 /* GET ALL
    Endpoint: /api/items
    Request Type:  GET
    Return: JSON Array of all items in database
 */
-
 
 app.get("/api/items", (req, res) =>
 {
@@ -75,12 +118,11 @@ app.get("/api/items", (req, res) =>
         {
             if(results.length === 0)
             {
-                console.log("No results found")
-                res.send("No Results found")
+               
+                res.status(500).send(msg500)
             }
             else
             {    
-                console.log(results)
                 res.send(results)
             }
         }
@@ -111,12 +153,14 @@ app.get("/api/items/:item_name", (req, res) =>{
             }
             else
             {    
+               
                 res.send(results)
+                
             }
         }
     ).catch(
         (error) => {
-            console.log("SHT")
+            res.status(500).send(msg500)
         }
     )
 
@@ -130,17 +174,29 @@ app.get("/api/items/:item_name", (req, res) =>{
 
 
  
-/*
-const s01 = new Items({"name": "Bob", "rarity": "common", "description": "POL", "goldPerTurn":1})
-s01.save().then(
-    console.log("Inserted")
-).catch(
-    (error) => {
-        console.log("error")
-        console.log(error)
-    }
-)
- */ 
+app.post("/api/items", (req, res) => { 
+     console.log("Recevied from client")
+     console.log(req.body)
+   
+   if(req.body.hasOwnProperty("name") === true || req.body.hasOwnProperty("rarity") === true)
+   {
+        Items.create(req.body).then(
+            (result) =>  {
+                console.log("Create success!")
+                res.status(201).send(msg201)
+            }
+        ).catch(
+            (err) => 
+            {
+                    res.status(500).send(msg500)
+            }
+        )
+  }
+  else
+  {
+    res.status(500).send("An internal error occured on the server side")
+  }
+})
 
 /* DELETE AN ITEM BY NAME
    Endpoint: /api/items:item_name
@@ -148,27 +204,30 @@ s01.save().then(
    Return: Delete an item by name
 */
 
-/*
-Items.deleteOne({name: "Bob"}).exec().then(
-    (deletedStudent) => 
-    {
-        if(deletedStudent == null)
-        {
-            console.log("Could not find a student to delete")
-        }
-        else
-        {
-            console.log(deletedStudent)
-        }
-    }
-).catch(
-    (err) => 
-    {
-        console.log(err)
-    }
-)
+app.delete("/api/items/:item_name", (req, res) => {
 
-*/
+    Items.deleteOne({name:req.params.item_name}).exec().then(
+        (deletedStudent) => 
+        {
+            if(deletedStudent == null)
+            {
+                res.status(500).send("Name not found")
+                
+            }
+            else
+            {
+                res.status(200).send("Successfully deleted")
+            }
+        }
+    ).catch(
+        (err) => 
+        {
+            res.status(500).send(msg500)
+        }
+    )
+})
+
+
 
 /*  UPDATE BY ID
    Endpoint: /api/items:item_id
@@ -176,11 +235,15 @@ Items.deleteOne({name: "Bob"}).exec().then(
    Return: Error code 404
 */
 
-/*
+app.put("/api/items/:item_id", (req, res) => 
+    {
+        res.status(501).send("The requested endpoint is currently not avaible, but my be implemented in the future")
+    }
+)
 
-*/
-
-   
-
+// Are out lost? 
+app.use(function(req,res){
+    res.status(404).send("Are you lost? Not found. ");
+});
 
 
